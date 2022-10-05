c7c60c0dfaf0ffc95b44558e3477f8cf130dd66e
frannavg <fran.nav.g@gmail.com>
2022-10-05 18:44:07 -0300

    Botao/ Toast/ Login_API_Musixmatch configurados

--- SoundTracker/src/components/QueryInput/QueryInput.tsx
+++ SoundTracker/src/components/QueryInput/QueryInput.tsx
@@ -1,25 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import './QueryInput.scss';
+import axios from "axios";
+import "react-toastify/dist/ReactToastify.css";
+import { toast, ToastContainer, Zoom, Bounce } from "react-toastify";
 
 export function QueryInput(){
+    /*API linkeys*/
+    const API_KEY: string = "53f18fdc77538571811fdbb46d965d72";
+    
+    /*States*/
+    const [query, setQuery] = useState("");
 
-/*     function onInputchange(e) {
-        this.setState({
-          [e.target.name]: e.target.value
-        });
-      } */
+    /*Handle Functions*/
+    function handleClick(event: any): void {
+        event.preventDefault();
+        console.log("handleClick called", event, query)
+        if (query.length === 0){
+            console.log("No query selected");
+            toast.error("Please enter a track !");
+            return;
+        }
+        else{
+            toast.success("Check your results below !");
+        }
+            
+    }
+
+    function handleChange(event: any): void {
+        event.preventDefault();
+        setQuery(event.target.value);
+        console.log(query);
+    }
 
     return (
         <div className="containerQuery">
+            <ToastContainer/>
             <label className="queryLabel">
                 Your Query
-                <input 
-                        className="queryInput"
-                        name="inputMusic"
-                        type="text"
-                />
+                <div className="queryButtonWrapper" style={{display:"flex"}}>
+                    <input 
+                            className="queryInput"
+                            name="inputMusic"
+                            type="text"
+                            onChange={handleChange}
+                            value={query}
+                            autoComplete="off"
+                    />
+                    <button 
+                            className="buttonInput"
+                            onClick={handleClick}>
+                        Search
+                    </button>
+                </div>
             </label>
-            <button >Search</button>
         </div>
     )
 }
